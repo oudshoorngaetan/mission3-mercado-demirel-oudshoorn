81621eb6eed503a44e174930e228fe467e18119e
oudshoorngaetan <gaetan.oudshorn@gmail.com>
2022-03-21 16:32:40 +0100

    modif affichage tableau biens

--- Mission3/modeles/accesBiens.php
+++ Mission3/modeles/accesBiens.php
@@ -1,7 +1,7 @@
 <?php 
 function getLesBiens($pdo) {
     $insert = $pdo->prepare("SELECT biens.id,ville,type.libelle,"
-            . "Prix,Superficie,NBpieces FROM biens "
+            . "Prix FROM biens "
             . "JOIN type ON type.id=biens.idType");
     $execute = $insert->execute();
     $lesBiens = $insert->fetchAll();
