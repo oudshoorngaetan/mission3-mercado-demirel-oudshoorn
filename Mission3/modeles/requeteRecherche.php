b62c60eaf7637256e0c338fba76b163f04085d60
oudshoorngaetan <gaetan.oudshorn@gmail.com>
2022-03-29 10:36:45 +0200

    modificiation css, menu et requeteRecherche.php

--- Mission3/modeles/requeteRecherche.php
+++ Mission3/modeles/requeteRecherche.php
@@ -3,19 +3,19 @@
 function rechercheBiens($pdo, $type, $ville) {
     $type = (int) $type;
     if ($ville != "" && $type != 0) {
-        $insert = $pdo->prepare("SELECT * FROM biens JOIN type ON type.id=biens.idType WHERE ville=:ville AND ID=:type");
+        $insert = $pdo->prepare("SELECT biens.ID,ville,libelle,prix FROM biens JOIN type ON type.id=biens.idType WHERE ville=:ville AND type.ID=:type");
         $bind1 = $insert->bindValue(":ville", $ville, PDO::PARAM_STR);
         $bind2 = $insert->bindValue(":type", $type);
     } else {
         if ($ville != "" && $type == 0) {
-            $insert = $pdo->prepare("SELECT * FROM biens JOIN type ON type.id=biens.idType WHERE ville=:ville");
+            $insert = $pdo->prepare("SELECT biens.ID,ville,libelle,prix FROM biens JOIN type ON type.id=biens.idType WHERE ville=:ville");
             $bind1 = $insert->bindValue(":ville", $ville, PDO::PARAM_STR);
         }
         if ($ville == "" && $type != 0) {
-            $insert = $pdo->prepare("SELECT * FROM biens JOIN type ON type.id=biens.idType WHERE ID=:type");
+            $insert = $pdo->prepare("SELECT biens.ID,ville,libelle,prix FROM biens JOIN type ON type.id=biens.idType WHERE type.ID=:type");
             $bind2 = $insert->bindValue(":type", $type);
         } else {
-            $insert = $pdo->prepare("SELECT * FROM biens JOIN type ON type.id=biens.idType");
+            $insert = $pdo->prepare("SELECT biens.ID,ville,libelle,prix FROM biens JOIN type ON type.id=biens.idType");
         }
     }
     $execute = $insert->execute();
