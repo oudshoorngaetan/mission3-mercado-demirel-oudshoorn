9adb55e3befb3e820a9baeb02bd956e93263ae2b
oudshoorngaetan <gaetan.oudshorn@gmail.com>
2023-04-11 15:45:02 +0200

    Correction suppression

--- Mission3/modeles/fonctionSupprimerBien.php
+++ Mission3/modeles/fonctionSupprimerBien.php
@@ -1,6 +1,7 @@
 <?php
 function supprimerBien($pdo,$ID){
-    $insert = $pdo->prepare("DELETE FROM biens WHERE ID=:id");
+    $insert = $pdo->prepare("DELETE FROM images WHERE IDbien=:id;
+                             DELETE FROM biens WHERE ID=:id");
     $bind = $insert->bindValue(":id", $ID, PDO::PARAM_INT);
     $execute = $insert->execute();
     return $execute;
